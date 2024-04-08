9bdb25823f1d59f10f0235eaf16feddd456fdf16
yairme <yaistward2013@gmail.com>
2024-04-08 11:35:52 +0200

    Refactor InteractableObject class to improve touch detection

--- GemeenteAmsterdam/Assets/Scripts/InteractableObject.cs
+++ GemeenteAmsterdam/Assets/Scripts/InteractableObject.cs
@@ -7,30 +7,20 @@ using System;
 public class InteractableObject : MonoBehaviour
 {
     [SerializeField] private int Radius;
-    [SerializeField] private bool isActive;
     [SerializeField] UnityEvent interactEvent;
+    private bool isActive;
+    public bool IsActive { get { return isActive; } set { isActive = value; } }
 
     void Start()
     {
-        if (interactEvent == null) // To prevent null reference exception
-        {
-            interactEvent = new UnityEvent(); // Create a new UnityEvent
-        }
-
+        if (interactEvent == null) { interactEvent = new UnityEvent(); }
     }
     // Update is called once per frame
     void Update() 
     {         
-        // if ( Radius < 0) // ToDo: Implement a better way to check if the player is in range.
-        // { 
-        //    if (!isActive) { this.gameObject.SetActive(true); }
-        // } 
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase == TouchPhase.Began) { interactEvent.Invoke(); }
-        }
+        if (!isActive) return;
+        foreach (Touch touch in Input.touches) { if (touch.phase == TouchPhase.Began) { interactEvent.Invoke(); } }
     }
     public void Ping() { Debug.Log("Ping"); } //Test listener
     public void Pong() { Debug.Log("Pong"); } // Test listener
-
 }
